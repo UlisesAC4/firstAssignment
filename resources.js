6ab0acd995f3993b20a17aef8719467f66fadabd
Ulises <ulisesavila04@gmail.com>
2017-02-22 23:56:17 -0600

    All buttons have the waves effect on click now, close on click on the mobile navbar was eliminated to not confuse the user

--- resources.js
+++ resources.js
@@ -1,9 +1,11 @@
 //custom JS code
 //for parallax initialization
 $(document).ready(function(){
+
   $('.parallax').parallax();
+
   $(".button-collapse").sideNav({
-    closeOnClick: true,
     draggable: true
   });
+
 });
